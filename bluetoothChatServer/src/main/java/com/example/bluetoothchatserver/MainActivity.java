6e6e7b24e21d9aa9903036f9284d61c809084e89
fly263 <dream1982@163.com>
2019-01-28 16:38:56 +0800

    自动开启“可发现”

--- bluetoothChatServer/src/main/java/com/example/bluetoothchatserver/MainActivity.java
+++ bluetoothChatServer/src/main/java/com/example/bluetoothchatserver/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.bluetoothchatserver;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setDiscoverableTimeout(100);
+    }
+
+    public void setDiscoverableTimeout(int timeout) {
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        try {
+            Method setDiscoverableTimeout = BluetoothAdapter.class.getMethod("setDiscoverableTimeout", int.class);
+            setDiscoverableTimeout.setAccessible(true);
+            Method setScanMode = BluetoothAdapter.class.getMethod("setScanMode", int.class, int.class);
+            setScanMode.setAccessible(true);
+            setDiscoverableTimeout.invoke(adapter, timeout);
+            setScanMode.invoke(adapter, BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE, timeout);
+        } catch (Exception e) {
+            Log.d("test", "setDiscoverableTimeout: "+e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
